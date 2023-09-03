d435fd23592e24b2b2d4e5c813add285449b5a0b
VectorProgra <2329723676@qq.com>
2023-09-03 19:55:11 +0800

    Initial commit

---
+++ src/main/java/com/example/gitrtestjava/GitRTestJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitrtestjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRTestJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitRTestJavaApplication.class, args);
+    }
+
+}
