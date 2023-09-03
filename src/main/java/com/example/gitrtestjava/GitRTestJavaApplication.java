866eef2740ffd314cf92b807eacfc39fd89cdb22
VectorProgra <2329723676@qq.com>
2023-09-03 19:57:14 +0800

    chushiu

--- src/main/java/com/example/gitrtestjava/GitRTestJavaApplication.java
+++ src/main/java/com/example/gitrtestjava/GitRTestJavaApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitRTestJavaApplication {
 
     public static void main(String[] args) {
+        System.out.println(111);
         SpringApplication.run(GitRTestJavaApplication.class, args);
+
     }
 
 }
